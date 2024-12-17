8e2d957e35f33a958fb8dfd528a4f8592a7430f6
Indrajith <indrajith.bodinayaka352@gmail.com>
2024-12-17 09:53:10 +0530

    Create propery and user model to interact with database

---
+++ models/User.js
@@ -0,0 +1,27 @@
+import {Schema, model, models} from 'mongoose';
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        unique: [true, "Email already exists"],
+        required: [true, "Email is required"]
+    },
+    userName: {
+        type: String,
+        required: [true, "Username is required"]
+    },
+    image: {
+        type: String,
+    },
+    bookmarks: [ 
+    {
+        type: Schema.Types.ObjectId,
+        ref: "Property"
+    }
+    ]
+}, {
+    timestapmes: true
+});
+
+const User = models.User || model("User", userSchema);
+export default User;
