60a35b90da59358af5c053df5aa793a7ec3ec03c
Indrajith <indrajith.bodinayaka352@gmail.com>
2024-12-27 11:40:58 +0530

    User profile listing

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -14,6 +14,8 @@ const ProfilePage = async () => {
         throw new Error("user id is required")
     }
 
+    const properties = await Property.find({owner: userId}).lean();
+
 
     return (
         <section className="bg-blue-50">
@@ -44,62 +46,37 @@ const ProfilePage = async () => {
 
             <div className="md:w-3/4 md:pl-4">
               <h2 className="text-xl font-semibold mb-4">Your Listings</h2>
-              <div className="mb-10">
-                <a href="/property.html">
-                  <Image
-                    className="h-32 w-full rounded-md object-cover"
-                    src="/images/properties/b1.jpg"
-                    width={400}
-                    height={400}
-                    alt="Property 1"
-                  />
-                </a>
-                <div className="mt-2">
-                  <p className="text-lg font-semibold">Property Title 1</p>
-                  <p className="text-gray-600">Address: 123 Main St</p>
+              {properties && properties.map((property) => (
+                <div className="mb-10" key={property._id}>
+                    <a href={`/properties/${property._id}`}>
+                    <Image
+                        className="h-32 w-full rounded-md object-cover"
+                        src={property.images[0]}
+                        width={1000}
+                        height={200}
+                        alt="Property 1"
+                    />
+                    </a>
+                    <div className="mt-2">
+                    <p className="text-lg font-semibold">{property.name}</p>
+                    <p className="text-gray-600">Location: {property.location.street} {property.location.city} {property.location.state}</p>
+                    </div>
+                    <div className="mt-2">
+                    <a
+                        href="/add-property.html"
+                        className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
+                    >
+                        Edit
+                    </a>
+                    <button
+                        className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
+                        type="button"
+                    >
+                        Delete
+                    </button>
+                    </div>
                 </div>
-                <div className="mt-2">
-                  <a
-                    href="/add-property.html"
-                    className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
-                  >
-                    Edit
-                  </a>
-                  <button
-                    className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
-                    type="button"
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
-              <div className="mb-10">
-                <a href="/property.html">
-                  <img
-                    className="h-32 w-full rounded-md object-cover"
-                    src="/images/properties/b1.jpg"
-                    alt="Property 2"
-                  />
-                </a>
-                <div className="mt-2">
-                  <p className="text-lg font-semibold">Property Title 2</p>
-                  <p className="text-gray-600">Address: 456 Elm St</p>
-                </div>
-                <div className="mt-2">
-                  <a
-                    href="/add-property.html"
-                    className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
-                  >
-                    Edit
-                  </a>
-                  <button
-                    className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
-                    type="button"
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
