ac4ac84d102879d1585458b653d1a028af45b88a
Indrajith <indrajith.bodinayaka352@gmail.com>
2025-01-03 16:58:03 +0530

    add left side bar section for bookmark, share and contact

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,8 @@
 import PropertyDetails from "@/components/PropertyDetails";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import BookmarkButton from "@/components/BookmarkButton";
+import ShareButtons from "@/components/ShareButtons";
+import PropertyContactForm from "@/components/PropertyContactForm";
 import PropertyImages from "@/components/PropertyImages";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
@@ -26,6 +29,11 @@ const PropertyPage = async({params}) => {
             <div className="container m-auto py-10 px-6">
             <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
                 <PropertyDetails property={property}/>
+                <aside className='space-y-4'>
+                    <BookmarkButton property = {property} />
+                    <ShareButtons property = {property} />
+                    <PropertyContactForm property = {property} />
+                </aside>
             </div>
             </div>
         </section>
