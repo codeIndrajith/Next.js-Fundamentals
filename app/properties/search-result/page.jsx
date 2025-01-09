964abfae6435c1ca53fc70fc53d3d22fe0607e4a
Indrajith <indrajith.bodinayaka352@gmail.com>
2025-01-09 11:33:13 +0530

    display search result

--- app/properties/search-result/page.jsx
+++ app/properties/search-result/page.jsx
@@ -1,6 +1,10 @@
+import Link from "next/link";
+import PropertyCard from "@/components/PropertyCard";
+import PropertySearchForm from "@/components/PropertySearchForm";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import { converToSerializableObject } from "@/utils/convertToObject";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 
 const SearchResultPage = async ({searchParams: {location, propertyType}}) => {
     await connectDB();
@@ -25,9 +29,30 @@ const SearchResultPage = async ({searchParams: {location, propertyType}}) => {
     const propertiesQueryResults = await Property.find(query).lean();
     const properties = converToSerializableObject(propertiesQueryResults);
 
-    console.log(properties)
     return (
-        <div>search reslut page</div>
+        <>
+            <section className="bg-blue-700 py-4">
+                <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lgpx-8">
+                    <PropertySearchForm />
+                </div>
+            </section>
+            <section className="px-4 py-6">
+                <Link href='/properties' className="flex items-center text-blue-500 hover:underline mb-3">
+                    <FaArrowAltCircleLeft className="mr-2 mb-1" /> Back to Properties
+                </Link>
+
+                <h1 className="text-2xl mb-4">Search Results</h1>
+                {properties.length === 0 ? (
+                    <p>No Search Results</p>
+                ) : (
+                    <div className="gird grid-cols-1 md:grid-cols-3 gap-6">
+                        {properties.map((property) => (
+                            <PropertyCard key={property._id} property={property} />
+                        ))}
+                    </div>
+                )}
+            </section>
+        </>
     )
 }
 
