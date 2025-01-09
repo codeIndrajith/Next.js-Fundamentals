63c7d78579cca7f4d0a19f0d5e3bbdb876331687
Indrajith <indrajith.bodinayaka352@gmail.com>
2025-01-09 11:20:06 +0530

    Fetch search results

--- app/properties/search-result/page.jsx
+++ app/properties/search-result/page.jsx
@@ -1,4 +1,31 @@
-const SearchResultPage = () => {
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { converToSerializableObject } from "@/utils/convertToObject";
+
+const SearchResultPage = async ({searchParams: {location, propertyType}}) => {
+    await connectDB();
+    const locationPattern = new RegExp(location , 'i');
+
+    let query = {
+        $or: [
+            {name: locationPattern},
+            {description: locationPattern},
+            {'location.stree': locationPattern},
+            {'location.city': locationPattern},
+            {'location.state': locationPattern},
+            {'location.zipcode': locationPattern},
+        ]
+    }
+
+    if(propertyType && propertyType !== "All") {
+        const typePattern = new RegExp(propertyType, 'i');
+        query.type = typePattern;
+    }
+
+    const propertiesQueryResults = await Property.find(query).lean();
+    const properties = converToSerializableObject(propertiesQueryResults);
+
+    console.log(properties)
     return (
         <div>search reslut page</div>
     )
